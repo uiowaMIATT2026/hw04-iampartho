c57494673477d3d1414fbda1f3125f2f216e138a
iampartho <partho.ndc@gmail.com>
2026-03-08 20:41:22 -0500

    Completed, compiled, and tested SimpleThreshold

---
+++ hw04-build/src/SimpleThreshold/CMakeFiles/SimpleThreshold_parthghosh.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleThreshold_parthghosh.dir/SimpleThreshold.cxx.o"
+  "CMakeFiles/SimpleThreshold_parthghosh.dir/SimpleThreshold.cxx.o.d"
+  "SimpleThresholdCLP.h"
+  "SimpleThreshold_parthghosh"
+  "SimpleThreshold_parthghosh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleThreshold_parthghosh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hw04-build/src/SimpleThreshold/CMakeFiles/SimpleThreshold_parthghosh.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SimpleThreshold_parthghosh.
+# This may be replaced when dependencies are built.
